19dda47c5ff835a5c84aac5f44802338011aac27
André Rodrigues <andre.rodriguesneves@gmail.com>
2021-06-07 23:13:10 -0300

    Atualização costumeira

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # código acima omitido
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static")
+#]
 
-LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "index"
+#LOGIN_URL = "login"
+#LOGIN_REDIRECT_URL = "index"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
