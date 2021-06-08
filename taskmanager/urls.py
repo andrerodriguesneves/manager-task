edf545cb9112b8818677b559c104855f5148391b
André Rodrigues <andre.rodriguesneves@gmail.com>
2021-06-07 23:05:36 -0300

    Atualização costumeira

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    #path('login', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('', taskmanager.core.views.index, name='index'),
 
 ]
