d45c4d3a66abde8f6788c09ad1e978781ef851a3
André Rodrigues <andre.rodriguesneves@gmail.com>
2021-06-19 19:29:23 -0300

    Criação da app de usuarios e alteração do models das tasks

--- taskmanager/task/models.py
+++ taskmanager/task/models.py
@@ -12,18 +12,21 @@ class Task(models.Model):
         ('DS', 'Daniel Santos')
     ]
 
+    TYPE_TASK = [('system change', 'Alteração de Sistema'),
+                 ('problem', 'Problema')]
     PRIORITY = [
         ('LOW', 'Baixa'),
         ('AVERAGE', 'Média'),
         ('HIGH', 'Alta')
     ]
 
-    title = models.CharField(max_length=150, blank=True,  verbose_name="Título")
-    description = models.TextField(max_length=4000, blank=True, verbose_name="Descrição")
-    solution = models.TextField(max_length=4000, null=True, blank=True, verbose_name="Solução")
+    title = models.CharField(max_length=300, blank=True,  verbose_name="Título")
+    description = models.TextField(max_length=8000, blank=True, verbose_name="Descrição")
+    type_task = models.CharField(max_length=50, blank=True, choices=TYPE_TASK, verbose_name="Tipo da Tarefa")
     responsible = models.CharField(max_length=50, choices=RESPONSIBLES_DEFAULT, verbose_name="Responsável")
     priority = models.CharField(max_length=50, choices=PRIORITY, verbose_name="Prioridade")
     created_in = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    solution = models.TextField(max_length=8000, null=True, blank=True, verbose_name="Solução")
     finalized_in = models.DateTimeField(null=True, blank=True, verbose_name="Finalizado em")
 
     class Meta:
